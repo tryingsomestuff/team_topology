7eac532482a22eb664e08331db8c86d95afc1a80
Vivien Clauzon <xr_a_y@yahoo.fr>
2025-02-27 21:27:01 +0100

    add fake_it curl script

---
+++ fake_it.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+TEAM_LIST_FROM=("ED" "SOLV" "SIMU" "SOFT" "DATA")
+TEAM_LIST=("ED" "SOLV" "SIMU" "SOFT" "DATA" "B2B" "B2C" "ADV" "PM" "MAT" "DOTI/BS" "DOTI/IN" "DOMF" "Others")
+
+# Select a random team
+SELECTED_TEAM=${TEAM_LIST_FROM[$RANDOM % ${#TEAM_LIST_FROM[@]}]}
+
+# Select a random number of connections (between 1 and 3 for variety)
+NUM_CONNECTIONS=$((1 + RANDOM % 3))
+SELECTED_CONNECTIONS=()
+
+# Pick random connections (excluding the selected team itself)
+for ((i = 0; i < NUM_CONNECTIONS; i++)); do
+    RANDOM_TEAM=${TEAM_LIST[$RANDOM % ${#TEAM_LIST[@]}]}
+    [[ "$RANDOM_TEAM" != "$SELECTED_TEAM" ]] && SELECTED_CONNECTIONS+=("\"$RANDOM_TEAM\"")
+done
+
+# Convert connections to JSON array format
+CONNECTIONS_JSON=$(IFS=,; echo "[${SELECTED_CONNECTIONS[*]}]")
+
+# Make the curl request
+curl -X POST "http://localhost:3000/api/vote" \
+     -H "Content-Type: application/json" \
+     -d "{\"team\": \"$SELECTED_TEAM\", \"connections\": $CONNECTIONS_JSON}"
+
